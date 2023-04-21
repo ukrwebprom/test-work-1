5a306a0a6a04380552638f488d6f8357653c53c0
ukrwebprom <ukrwebprom@gmail.com>
2023-04-22 01:35:42 +0300

    basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/test-work-1'>
     <App />
     </BrowserRouter>
   </React.StrictMode>
