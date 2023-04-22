4035f53143d58bf8da227fcf164bcfc768d18735
ukrwebprom <ukrwebprom@gmail.com>
2023-04-23 01:38:31 +0300

    /tweets link

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,7 +4,7 @@ export const Home = () => {
     return (
         <Hero>
             <h1>TEST WORK <i>#1</i></h1>
-            <Link to="/tweets">Tweets</Link>
+            <Link to="tweets">Tweets</Link>
         </Hero>
         
     )
