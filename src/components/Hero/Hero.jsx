3e7855291316ce2a92525d9ec8f27804318acfa8
ukrwebprom <ukrwebprom@gmail.com>
2023-04-23 02:49:39 +0300

    mainpage styles

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,9 +3,7 @@ import './hero.scss';
 export const Hero = ({children}) => {
     return (
         <div className="hero">
-            <div className="wrapper">
                 {children}
-            </div>
         </div>
     )
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,6 +4,9 @@ export const Home = () => {
     return (
         <Hero>
             <h1>TEST WORK <i>#1</i></h1>
+            <h2>for a position of<br />Junior FrontEnd Developer</h2>
+            <p><strong>Used technologies</strong></p>
+            <p>React, SCSS, Material UI, CLSX, AXIOS</p>
             <Link to="tweets">Tweets</Link>
         </Hero>
         
